349a12ff4b6e0e2800ba05201d9495d8616730a9
erictria <ericmtria@gmail.com>
2024-01-15 21:24:21 -0500

    Add initial UI files for the flask app

--- flask/main.py
+++ flask/main.py
@@ -1,14 +1,22 @@
 from flask import (
     Flask,
+    render_template,
     request,
     jsonify
 )
+from livereload import Server
 import pandas as pd
+
 from blueprints.himdex_blueprint import himdex_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
 app.register_blueprint(himdex_blueprint)
 
+@app.route('/')
+def index():
+    return render_template('landing.html')
+
 if __name__ == '__main__':
-    app.run('localhost', load_dotenv = True)
+    server = Server(app.wsgi_app)
+    server.serve(port=5000, debug=False, restart_delay=0)
